2bb4aa79c7d3b9d9b58fa2fea46a0e5f819319b9
nhishaaulia <nhishaaulia94@gmail.com>
2025-08-07 14:59:17 +0700

    tugasflutter6

--- lib/Tugas Flutter/tugas_2.dart
+++ lib/Tugas Flutter/tugas_2.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Tugas2 extends StatelessWidget {
-  const Tugas2 ({super.key});
+  const Tugas2({super.key});
 
- final String  description = 'Hiii! Nama saya Nhisha Aulia ^^\n\Lulusan Sistem Informasi dari Universitas Bakrie. Saya aktif dalam organisasi dan berbagai proyek selama kuliah yang mengasah kemampuan komunikasi, manajemen waktu, dan kerja tim. Saya seorang individu yang berdedikasi, teliti, dan memiliki semangat belajar tinggi. Memiliki pengalaman dalam teknologi informasi dan mampu bekerja secara mandiri maupun dalam tim. Terbiasa menghadapi tantangan baru dengan pendekatan yang solutif dan komunikatif. Siap belajar dan berkontribusi secara maksimal di dunia profesional.';
- 
- @override
+  final String description =
+      'Hiii! Nama saya Nhisha Aulia ^^\nLulusan Sistem Informasi dari Universitas Bakrie. Saya aktif dalam organisasi dan berbagai proyek selama kuliah yang mengasah kemampuan komunikasi, manajemen waktu, dan kerja tim. Saya seorang individu yang berdedikasi, teliti, dan memiliki semangat belajar tinggi. Memiliki pengalaman dalam teknologi informasi dan mampu bekerja secara mandiri maupun dalam tim. Terbiasa menghadapi tantangan baru dengan pendekatan yang solutif dan komunikatif. Siap belajar dan berkontribusi secara maksimal di dunia profesional.';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -17,67 +18,79 @@ class Tugas2 extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children:
-           [
+          children: [
             //Nama Lengkap
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                  CircleAvatar(
-                    radius: 50,
-                  ), 
+                  CircleAvatar(radius: 50),
                   SizedBox(height: 8),
-                  Text("Nhisha Aulia",style: TextStyle( fontSize: 30,fontWeight: FontWeight.bold,
-                    ),
+                  Text(
+                    "Nhisha Aulia",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
                 ],
               ),
             ),
-        
-        
+
             //Email
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal:20),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
               child: Container(
-                padding:EdgeInsets.all(12),
-                decoration:BoxDecoration(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
                   color: const Color.fromARGB(255, 236, 137, 174),
-                    border:Border.all(color: const Color.fromARGB(255, 78, 100, 110), width:3),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 78, 100, 110),
+                    width: 3,
+                  ),
                   borderRadius: BorderRadius.circular(15),
                 ),
                 child: Row(
                   children: [
-                    SizedBox(width:15),
+                    SizedBox(width: 15),
                     Icon(Icons.email, color: Colors.black),
-                    SizedBox(width:15),
-                    Text("nhishaauliappkdjp@gmail.com", style: TextStyle(fontSize: 18, color: Colors.black,),
-                  )],
-                )
-              )
+                    SizedBox(width: 15),
+                    Text(
+                      "nhishaauliappkdjp@gmail.com",
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
             ),
-        
+
             // Nomor HP dengan Spacer (rata kanan)
-             Padding(
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal:20),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
               child: Container(
-                padding:EdgeInsets.all(12),
-                decoration:BoxDecoration(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
                   color: const Color.fromARGB(255, 236, 137, 174),
-                    border:Border.all(color: const Color.fromARGB(255, 78, 100, 110), width:3),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 78, 100, 110),
+                    width: 3,
+                  ),
                   borderRadius: BorderRadius.circular(15),
                 ),
                 child: Row(
-                children: [
-                    SizedBox(width:15),
+                  children: [
+                    SizedBox(width: 15),
                     Icon(Icons.phone, color: Colors.black),
-                    SizedBox(width:15),
-                    Text("0812-3456-7890",style: TextStyle(fontSize: 18, color: const Color.fromARGB(255, 0, 0, 0),)
-                  )],
-                )
-              )
+                    SizedBox(width: 15),
+                    Text(
+                      "0812-3456-7890",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-        
+
             // Dua kotak sejajar
             Padding(
               padding: const EdgeInsets.all(16.0),
@@ -89,61 +102,66 @@ class Tugas2 extends StatelessWidget {
                       padding: EdgeInsets.all(12),
                       height: 55,
                       alignment: Alignment.center,
-                      width:200,
-                      decoration: BoxDecoration(color: const Color.fromARGB(255, 155, 185, 211),
-                      border: Border.all(color: const Color.fromARGB(255, 78, 100, 110), width: 3),
-                      borderRadius: BorderRadius.circular(12),),
-                      child: Text("Postingan", style: TextStyle(fontSize: 15, color: Colors.black,)),
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 155, 185, 211),
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 78, 100, 110),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
                       ),
+                      child: Text(
+                        "Postingan",
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                      ),
+                    ),
                   ),
-        
-        
+
                   SizedBox(width: 14),
                   Expanded(
                     child: Container(
                       padding: EdgeInsets.all(12),
                       height: 55,
                       alignment: Alignment.center,
-                      width:20,
-                      decoration: BoxDecoration(color: const Color.fromARGB(255, 151, 137, 192),
-                      border: Border.all(color: const Color.fromARGB(255, 78, 100, 110), width: 3),
-                      borderRadius: BorderRadius.circular(12),),
-                      child: Text("Followers", style: TextStyle(fontSize: 15, color: Colors.black,)),
+                      width: 20,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 151, 137, 192),
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 78, 100, 110),
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
                       ),
+                      child: Text(
+                        "Followers",
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-        
+
             // Deskripsi
             const SizedBox(height: 16),
             Center(
-              child: Text(
-                "Deskripsi Profil",
-                style: TextStyle(fontSize: 16),
-              ),
+              child: Text("Deskripsi Profil", style: TextStyle(fontSize: 16)),
             ),
-        
+
             // Hiasan visual bawah
             const SizedBox(height: 8),
             Padding(
               padding: const EdgeInsets.all(18),
               child: Container(
-                
                 // color: Colors.purple[100],
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                color: Colors.purple
-                    
-                  )
+                  border: Border.all(color: Colors.purple),
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    description, 
-                    style: TextStyle(fontSize: 16),
-                  ),
+                  child: Text(description, style: TextStyle(fontSize: 16)),
                 ),
               ),
             ),
---
+++ lib/Tugas Flutter/tugas_6.dart
@@ -0,0 +1,292 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Tugas6 extends StatefulWidget {
+  const Tugas6({super.key});
+
+  @override
+  State<Tugas6> createState() => _Tugas6State();
+}
+
+class _Tugas6State extends State<Tugas6> {
+  // final bool _isPasswordHidden = true;
+  final _fromKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _obsecureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login", style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: false,
+        leading: Icon(Icons.arrow_back_ios),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Form(
+          key: _fromKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome Back",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Sign In to your account",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
+              SizedBox(height: 30),
+
+              // Email Address
+              Text(
+                "Email Address",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: const Color.fromARGB(255, 141, 141, 141),
+                ),
+              ),
+              SizedBox(height: 15),
+
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  hintText: "Masukkan Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty)
+                    return "Email tidak boleh kosong";
+                  if (!value.contains('@')) return "Email tidak valid";
+                  return null;
+                },
+              ),
+
+              SizedBox(height: 25),
+
+              // Password
+              Text(
+                "Password",
+                style: TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+              SizedBox(height: 15),
+
+              TextFormField(
+                controller: _passwordController,
+                obscureText: _obsecureText,
+                decoration: InputDecoration(
+                  hintText: "Masukkan Password",
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _obsecureText = !_obsecureText;
+                      });
+                    },
+                    icon: Icon(
+                      _obsecureText ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty)
+                    return "Password tidak boleh kosong";
+                  return null;
+                },
+              ),
+
+              // Forgot Password
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 228, 139, 112),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25),
+
+              // Login
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_fromKey.currentState!.validate()) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text("Login Berhasil"),
+                                Lottie.asset(
+                                  "assets/images/animations/result page success motion design.json",
+                                ),
+                              ],
+                            ),
+                            content: Text(
+                              "Welcome Back Again",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            actions: [
+                              TextButton(
+                                child: Text("Lanjutkan"),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 53, 43, 202),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+
+              // Sign Up
+              Align(
+                child: Text.rich(
+                  TextSpan(
+                    text: "Don’t have an account?",
+                    style: TextStyle(fontSize: 13, color: Colors.grey),
+                    children: [
+                      TextSpan(
+                        text: "  Sign Up",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13,
+                          color: const Color.fromARGB(255, 228, 139, 112),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              // Sign In With
+              Row(
+                children: [
+                  Expanded(child: Divider()),
+                  SizedBox(width: 8),
+                  Text(
+                    "Or Sign In With",
+                    style: TextStyle(fontSize: 12.5, color: Colors.grey),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(child: Divider()),
+                ],
+              ),
+              SizedBox(height: 25),
+
+              // Icon Google & Facebook
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // Google
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      style: ButtonStyle(elevation: WidgetStateProperty.all(0)),
+                      onPressed: () {},
+                      icon: Image.asset(
+                        'assets/images/google_logo.png',
+                        height: 50,
+                        width: 24,
+                      ),
+                      label: Text(
+                        "Google",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+
+                  // Facebook
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      style: ButtonStyle(elevation: WidgetStateProperty.all(0)),
+                      onPressed: () {},
+                      icon: Image.asset(
+                        'assets/images/facebook_logo.png',
+                        height: 50,
+                        width: 24,
+                      ),
+                      label: Text(
+                        "Facebook",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 25),
+
+              // Join Us
+              Text.rich(
+                TextSpan(
+                  text: "Don’t have an account?",
+                  style: TextStyle(fontSize: 13, color: Colors.grey),
+                  children: [
+                    TextSpan(
+                      text: "  Join Us",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                        color: const Color.fromARGB(255, 228, 139, 112),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
